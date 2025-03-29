bb16bcedf72ad9cc02b19fd71bf312484e639111
Dohyun <104420796+chungguul@users.noreply.github.com>
2025-03-29 21:52:58 +0900

    게임 매니저 수정
    서버에서만 동작하도록 네트워크 처리 작업

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,16 +1,68 @@
 using System.Collections;
 using System.Collections.Generic;
+using Fusion;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour
+public enum GameState
 {
+    InProgress, // 라운드 진행중
+    Started, // 라운드 시작
+    Ended, // 라운드 종료
+
+}
+
+
+public class GameManager : NetworkBehaviour
+{
+    public float timeLimit = 60f;
+    
+    //Sriptable Object
     public GameStartEventSO gameStartEvent;
+
+    //Network Object
+    [Networked] public GameState State { get; private set; }
+    [Networked] public float Timer { get; private set; }
+
+    public override void FixedUpdateNetwork()
+    {
+        //클라이언트 제외외
+        if (!Runner.IsServer) return;
+
+        switch (State)
+        {
+            case GameState.Started:
+                StartRound();
+                break;
+            case GameState.InProgress:
+                Timer -= Runner.DeltaTime;
+
+                if (Timer <= 0f)
+                {
+                    EndRound();
+                } 
+                break;
+        }
+    }
+
+    void StartRound()
+    {
+        State = GameState.InProgress;
+        Timer = timeLimit;
+        Debug.Log("Round Started");
+    }
+
+    void EndRound()
+    {
+        State = GameState.Ended;
+        Debug.Log("Round Ended");
+    }
 
     private void Start()
     {
         Debug.Log("Game Started");
+        if (!Runner.IsServer) return;
 
-        // 이벤트 발생 원하는 특정 시점에 실행
+        Debug.Log("Thread Raise");
         gameStartEvent.Raise();
     }
 
