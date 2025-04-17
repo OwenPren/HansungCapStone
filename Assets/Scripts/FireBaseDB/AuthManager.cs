8561accef21b71d46f7fa17f97154c38227730e3
Dohyun <104420796+chungguul@users.noreply.github.com>
2025-04-17 10:26:07 +0900

    Update AuthManager.cs

--- Assets/Scripts/FireBaseDB/AuthManager.cs
+++ Assets/Scripts/FireBaseDB/AuthManager.cs
@@ -12,42 +12,42 @@ public class AuthManager : MonoBehaviour
     [SerializeField] TMP_InputField emailField;
     [SerializeField] TMP_InputField passField;
 
-    // ������ ������ ��ü
-    Firebase.Auth.FirebaseAuth auth;
+//// 인증을 관리할 객체
+    //Firebase.Auth.FirebaseAuth auth;
 
-    void Awake()
-    {
-        // ��ü �ʱ�ȭ
-        auth = Firebase.Auth.FirebaseAuth.DefaultInstance;
-    }
-    public void login()
-    {
-        // �����Ǵ� �Լ� : �̸��ϰ� ��й�ȣ�� �α��� ���� ��
-        auth.SignInWithEmailAndPasswordAsync(emailField.text, passField.text).ContinueWith(
-            task => {
-                if (task.IsCompleted && !task.IsFaulted && !task.IsCanceled)
-                {
-                    Debug.Log(emailField.text + " �� �α��� �ϼ̽��ϴ�.");
-                }
-                else
-                {
-                    Debug.Log("�α��ο� �����ϼ̽��ϴ�.");
-                }
-            }
-        );
-    }
-    public void register()
-    {
-        // �����Ǵ� �Լ� : �̸��ϰ� ��й�ȣ�� ȸ������ ���� ��
-        auth.CreateUserWithEmailAndPasswordAsync(emailField.text, passField.text).ContinueWith(
-            task => {
-                if (!task.IsCanceled && !task.IsFaulted)
-                {
-                    Debug.Log(emailField.text + "�� ȸ������\n");
-                }
-                else
-                    Debug.Log("ȸ������ ����\n");
-            }
-            );
-    }
+    //void Awake()
+    //{
+    //    // 객체 초기화
+    //    auth = Firebase.Auth.FirebaseAuth.DefaultInstance;
+    //}
+    //public void login()
+    //{
+    //    // 제공되는 함수 : 이메일과 비밀번호로 로그인 시켜 줌
+    //    auth.SignInWithEmailAndPasswordAsync(emailField.text, passField.text).ContinueWith(
+    //        task => {
+    //            if (task.IsCompleted && !task.IsFaulted && !task.IsCanceled)
+    //            {
+    //                Debug.Log(emailField.text + " 로 로그인 하셨습니다.");
+    //            }
+    //            else
+    //            {
+    //                Debug.Log("로그인에 실패하셨습니다.");
+    //            }
+    //        }
+    //    );
+    //}
+    //public void register()
+    //{
+    //    // 제공되는 함수 : 이메일과 비밀번호로 회원가입 시켜 줌
+    //    auth.CreateUserWithEmailAndPasswordAsync(emailField.text, passField.text).ContinueWith(
+    //        task => {
+    //            if (!task.IsCanceled && !task.IsFaulted)
+    //            {
+    //                Debug.Log(emailField.text + "로 회원가입\n");
+    //            }
+    //            else
+    //                Debug.Log("회원가입 실패\n");
+    //        }
+    //        );
+    //}
 }
